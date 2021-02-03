b8fd28cb2f7e70788e156b0b9fd3404fb4349539
Marko Markovic <marko.markovic.markovic.9@gmail.com>
2021-02-03 18:58:49 +0100

    Added wallet and block input

--- main.py
+++ main.py
@@ -4,6 +4,11 @@
 def main():
     print("== Ethereum transactions crawler ==")
 
+    w = input("Please enter wallet address:")
+    b = input("Please enter block to start from:")
+
+    print("\nSearching for block " + str(b) + " from wallet " + str(w) + "...\n")
+
 
 if __name__ == "__main__":
     main()
